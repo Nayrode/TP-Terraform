23f7a89a1be2f576b029dfdb62d410275061cc33
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-03-12 14:18:56 +0100

    modif

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-/*module "ec2" {
+module "ec2" {
   source  = "./modules/ec2"
   nom_instance = var.nom_instance
   type = var.type
   user_data = var.user_data
   nom_role = var.nom_role
-}*/
+}
 
 module "SNS" {
   source  = "./modules/SNS"

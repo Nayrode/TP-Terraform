7bc6459700dcccca6bb8609266a17fd7ea89ef27
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-03-12 14:58:54 +0100

    ca corrige des trucs

--- modules/SNS/resources.tf
+++ modules/SNS/resources.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "lambda" {
   function_name = var.lambda_function_name
   handler       = "exports.handler"
-  runtime       = "nodejs18.x"
+  runtime       = "python3.11"
   role          = var.lambda_role_arn
   filename      = "lambda.zip"
   publish       = true

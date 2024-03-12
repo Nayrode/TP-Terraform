ecf50e7ba34171e77794feda798bf6b05ac925f4
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-03-12 14:26:43 +0100

    default values

--- variables.tf
+++ variables.tf
@@ -1,44 +1,53 @@
 variable "nom_instance" {
   description = "Nom de l'instance EC2"
+  default = "instance-ec2"
   type    = string
 }
 
 variable "type" {
   description = "Type de l'instance EC2"
+  default = "t2.micro"
   type    = string
 }
 
 variable "user_data" {
   description = "Script de démarrage de l'instance EC2"
+  default = ""
   type    = string
 }
 
 variable "nom_role" {
   description = "Nom du role de l'instance EC2"
+  default = "EC2InstanceRole"
   type    = string
 }
 
 variable "sns_topic_name" {
   description = "Nom de la file SNS"
+  default = "QueueSNS"
   type        = string
 }
 
 variable "lambda_function_name" {
   description = "Nom de la fonction Lambda"
+  default = "LambdaFunction"
   type        = string
 }
 
 variable "subscriber_emails" {
   description = "Liste des emails des abonnés à la file SNS"
+  default     = [""]
   type        = list(string)
 }
 
 variable "notification_message" {
   description = "Notification a envoyer aux abonnés de la file SNS"
+  default     = "Notification"
   type        = string
 }
 
 variable "lambda_role_arn" {
   description = "ARN du role de la fonction Lambda"
+  default     = "arn:aws:iam::381492154596:role/LabRole"
   type        = string
 }
